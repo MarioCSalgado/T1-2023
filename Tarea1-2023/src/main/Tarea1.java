48e5c9c777eaeb8964af096f7e702e2c4ac573b4
mario_cso15@hotmail.com <Mario_cso15@hotmail.com>
2023-04-28 23:39:07 -0400

    No se modifico, solo lo subo de nuevo por un fallo al subir el Main en el anterior commmit

--- Tarea1-2023/src/main/Tarea1.java
+++ Tarea1-2023/src/main/Tarea1.java
@@ -1,100 +1,128 @@
 package main;
 
 public class Tarea1 {
+
     public static void main(String[] args) throws PagoIncorrectoException, PagoInsuficienteException, NoHayProductoException {
-        Expendedor exp = new Expendedor(4, 1000);
+        Expendedor exp = new Expendedor(4);
         Moneda m = null;
         Comprador c = null;
+        System.out.println("Precios productos: ");
+        System.out.println("Cocacola: " + Expendedor.precioCoca);
+        System.out.println("Sprite: " + Expendedor.precioSprite);
+        System.out.println("Super8: " + Expendedor.precioSuper8);
+        System.out.println("Trencito: " + Expendedor.precioTrencito + "\n");
 
-//-----Bebida que NO vende-----
-/*Lanza exception por que el codigo no esta en el deposito*/
-      /*m = new Moneda100();
-        c = new Comprador(m, 657, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());*/ //Si le sacamos los comentarios lanza NoHayProducotException
+//-----Producto que NO vende, lanza una exception y con try-catch lanzamos un msg en este, caso y devolvemos la moenda*/
+        try {
+            m = new Moneda100();
+            c = new Comprador(m, 657, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto()); //Si le sacamos los comentarios lanza NoHayProducotException
+        } catch (NoHayProductoException e) {
+            System.out.println("El codigo ingresado no corresponde a ningun producto, se le devolvera su moneda: " + exp.getVuelto().getValor());
+        }
+//-----trata de comprar SIN Moneda, lanza una exception y con try-catch devuelve el msg-----
+        try {
+            m = null;
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// Si le sacamos los comentarios lanza PagoIncorrectoException
+        } catch (PagoIncorrectoException e) {
+            System.out.println("No se ingreso ninguna moneda");
+        }
 
-//-----trata de comprar SIN Moneda-----
-    /*Lanza exception por que la moneda es null*/
-      /*  m = null;
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());*/// Si le sacamos los comentarios lanza PagoIncorrectoException
-
-
-//-----con dinero JUSTO para el precio-----
-        m = new Moneda1000();
-        c = new Comprador(m, Expendedor.COCA, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $0
-        m = new Moneda1000();
-        c = new Comprador(m, Expendedor.COCA, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $0
-        m = new Moneda1000();
-        c = new Comprador(m, Expendedor.COCA, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $0
-        m = new Moneda1000();
-        c = new Comprador(m, Expendedor.COCA, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $0
-
-//-----con MENOS dinero que el precio-----
-/*Lanza exception por que es menos dinero al precio*/
-        /* m = new Moneda500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());*///si le sacamos los comentarios lanza PagoInsuficienteException
-  
-
-
-//-----con MAS dinero que el precio-----
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);    
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $500
-
-//-----Deposito VACIO-----
-       /*Lanza Exception por que no hay producto*/
-       /*m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());*/
-      //si le sacamos los comentarios lanza NoHayProductoException 
+//-----con dinero JUSTO para el precio y y si esta vacio retorna una exception, usamos try-catch para imprimir un msg-----
+        try {
+            m = new Moneda1000();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $100
+            m = new Moneda1000();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $100
+            m = new Moneda1000();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $100
+            m = new Moneda1000();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $100
+            m = new Moneda1000();
+            c = new Comprador(m, Expendedor.COCA, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $100
+        } catch (NoHayProductoException e) {
+            System.out.println("No quedan Cocacola's, se le devolvera su moneda: " + exp.getVuelto().getValor());
+        }
         
-        
-//-----Con Mas dinero para comprar Super8-----
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SUPER8, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// super8 $500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SUPER8, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// super8 $500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SUPER8, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// super8 $500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SUPER8, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// super8 $500
+//-----con MENOS dinero que el precio, lanza una exception y usamos try-catch para devolver un msg y la moneda-----
+        try {
+            m = new Moneda500();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());//si le sacamos los comentarios lanza PagoInsuficienteException
+        } catch (PagoInsuficienteException e) {
+            System.out.println("No le alcanzaba para comprar el producto, se le devolvera su moneda: " + exp.getVuelto().getValor());
+        }
 
-//-----Con Mas dinero para comprar Trencito-----
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.TRENCITO, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito $500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.TRENCITO, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito 500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.TRENCITO, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito $500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.TRENCITO, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito $500
+//-----con MAS dinero que el precio y si esta vacio retorna una exception, usamos try-catch para imprimir un msg-----
+        try {
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $600
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $600
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $600
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $600
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SPRITE, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $600
+        } catch (NoHayProductoException e) {
+            System.out.println("No quedan Sprite's, se le devolvera su moneda: " + exp.getVuelto().getValor());
+        }
+
+//-----Con Mas dinero para comprar Super8 y si esta vacio retorna una exception, usamos try-catch para imprimir un msg-----
+        try {
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SUPER8, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// super8 $1200
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SUPER8, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// super8 $120
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SUPER8, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// super8 $1200
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SUPER8, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// super8 $1200
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.SUPER8, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("No quedan Super8's, se le devolvera su moneda: " + exp.getVuelto().getValor());
+        }
         
-       /*Lanza NoHayProductoExpcetion, si le sacamos los comentarios, ya que supera el numero del expendedor*/
-       /* m = new Moneda1500();
-        c = new Comprador(m, Expendedor.TRENCITO, exp);
-        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());*/
+//-----Con Mas dinero para comprar Trencito y si esta vacio retorna una exception, usamos try-catch para imprimir un msg-----
+        try {
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.TRENCITO, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito $300
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.TRENCITO, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito 300
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.TRENCITO, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito $300
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.TRENCITO, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito $300
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.TRENCITO, exp);
+            System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito $300
+
+        } catch (NoHayProductoException e) {
+            System.out.println("No quedan Trencito's, se le devolvera su moneda: " + exp.getVuelto().getValor());
+        }
+
         
     }
 }
