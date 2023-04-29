76f4beadb97c1fd8bee8156a9ed07d8b59a09190
mario_cso15@hotmail.com <Mario_cso15@hotmail.com>
2023-04-28 21:25:57 -0400

    Modifacion del Main(Tarea1), se modifico el main para que se entienda de mejor manera el codigo y explicar donde ocurren las excepciones con comentarios

--- Tarea1-2023/src/main/Tarea1.java
+++ Tarea1-2023/src/main/Tarea1.java
@@ -7,124 +7,94 @@ public class Tarea1 {
         Comprador c = null;
 
 //-----Bebida que NO vende-----
-        m = new Moneda1500();
+/*Lanza exception por que el codigo no esta en el deposito*/
+      /*m = new Moneda100();
         c = new Comprador(m, 657, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $500
-        m = new Moneda1500();
-        c = new Comprador(m, 657, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $500
-        m = new Moneda1500();
-        c = new Comprador(m, 657, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $500
-        m = new Moneda1500();
-        c = new Comprador(m, 657, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $500
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());*/ //Si le sacamos los comentarios lanza NoHayProducotException
 
 //-----trata de comprar SIN Moneda-----
-   /*    m = null;
+    /*Lanza exception por que la moneda es null*/
+      /*  m = null;
         c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $0
-        m = null;
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $0
-        m = null;
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $0
-        m = null;
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $0*/
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());*/// Si le sacamos los comentarios lanza PagoIncorrectoException
+
 
 //-----con dinero JUSTO para el precio-----
         m = new Moneda1000();
         c = new Comprador(m, Expendedor.COCA, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// cocacola $0
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $0
         m = new Moneda1000();
         c = new Comprador(m, Expendedor.COCA, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// cocacola $0
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $0
         m = new Moneda1000();
         c = new Comprador(m, Expendedor.COCA, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// cocacola $0
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $0
         m = new Moneda1000();
         c = new Comprador(m, Expendedor.COCA, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// cocacola $0
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// cocacola $0
 
 //-----con MENOS dinero que el precio-----
-   /*    m = new Moneda500();
+/*Lanza exception por que es menos dinero al precio*/
+        /* m = new Moneda500();
         c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $500
-        m = new Moneda500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $500
-        m = new Moneda500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $500
-        m = new Moneda500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $500
-*/
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());*///si le sacamos los comentarios lanza PagoInsuficienteException
+  
+
+
 //-----con MAS dinero que el precio-----
         m = new Moneda1500();
         c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// sprite $500
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $500
         m = new Moneda1500();
         c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// sprite $500
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $500
         m = new Moneda1500();
         c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// sprite $500
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $500
         m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// sprite $500
+        c = new Comprador(m, Expendedor.SPRITE, exp);    
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// sprite $500
 
 //-----Deposito VACIO-----
-       /* m = new Moneda1500();
+       /*Lanza Exception por que no hay producto*/
+       /*m = new Moneda1500();
         c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $1500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $1500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $1500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SPRITE, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// null $1500
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());//// null $1500*/
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());*/
+      //si le sacamos los comentarios lanza NoHayProductoException 
         
         
-        /*****************************/
+//-----Con Mas dinero para comprar Super8-----
         m = new Moneda1500();
         c = new Comprador(m, Expendedor.SUPER8, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// super8 $500
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// super8 $500
         m = new Moneda1500();
         c = new Comprador(m, Expendedor.SUPER8, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// super8 $500
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// super8 $500
         m = new Moneda1500();
         c = new Comprador(m, Expendedor.SUPER8, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// super8 $500
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// super8 $500
         m = new Moneda1500();
         c = new Comprador(m, Expendedor.SUPER8, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// super8 $500
-        /*m = new Moneda1500();
-        c = new Comprador(m, Expendedor.SUPER8, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// super8 $500*/
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// super8 $500
+
+//-----Con Mas dinero para comprar Trencito-----
+        m = new Moneda1500();
+        c = new Comprador(m, Expendedor.TRENCITO, exp);
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito $500
+        m = new Moneda1500();
+        c = new Comprador(m, Expendedor.TRENCITO, exp);
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito 500
+        m = new Moneda1500();
+        c = new Comprador(m, Expendedor.TRENCITO, exp);
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito $500
+        m = new Moneda1500();
+        c = new Comprador(m, Expendedor.TRENCITO, exp);
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());// trencito $500
         
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.TRENCITO, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// trencito $500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.TRENCITO, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// trencito 500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.TRENCITO, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// trencito $500
-        m = new Moneda1500();
-        c = new Comprador(m, Expendedor.TRENCITO, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// trencito $500
-        
+       /*Lanza NoHayProductoExpcetion, si le sacamos los comentarios, ya que supera el numero del expendedor*/
        /* m = new Moneda1500();
         c = new Comprador(m, Expendedor.TRENCITO, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());// sprite $500*/
+        System.out.println(c.queDegustaste() + ", " + c.cuantoVuelto());*/
         
     }
 }
