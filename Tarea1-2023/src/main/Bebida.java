87c7e411241abf70119629eb6f7a91a8d08a545e
mario_cso15@hotmail.com <Mario_cso15@hotmail.com>
2023-05-01 20:36:34 -0400

    Resubida Bebida preJavaDoc, por un inconvenite

--- Tarea1-2023/src/main/Bebida.java
+++ Tarea1-2023/src/main/Bebida.java
@@ -1,13 +1,8 @@
 
 package main;
-/**
-*Subclase abstracta Bebida,lleva el numero de serie de la bebida a la super clase Producto
-*/
+
+/********Bebida********/
 abstract class Bebida extends Producto{//no se puede instanciar directamente
-    /**
-    *Metodo constructor de Bebida, retorna el valor de serie recibido de sus subclases a su superclse Producto
-    *@param serie El numero de serie de la bebida
-    */ 
     public Bebida(int serie){
         super(serie);
     }  
