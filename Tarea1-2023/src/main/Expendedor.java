ddc3897a7f3e338821118a25dfcad43bf502730a
mario_cso15@hotmail.com <Mario_cso15@hotmail.com>
2023-05-01 20:22:46 -0400

    Actualizacion de Expendedor para los metodos genericos de Deposito

--- Tarea1-2023/src/main/Expendedor.java
+++ Tarea1-2023/src/main/Expendedor.java
@@ -1,11 +1,11 @@
 package main;
 
 class Expendedor{
-    private Deposito coca;
-    private Deposito sprite;
-    private Deposito super8;
-    private Deposito trencito;
-    private Deposito monVu;
+    private Deposito<Bebida> coca;
+    private Deposito<Bebida> sprite;
+    private Deposito<Dulce> super8;
+    private Deposito<Dulce> trencito;
+    private Deposito<Moneda> monVu;
     
     public static final int COCA=1; 
     public static final int SPRITE=2;
@@ -18,17 +18,17 @@ class Expendedor{
     public int vuelto=0;
    
     public Expendedor(int productos) {
-        coca = new Deposito();
-        sprite = new Deposito();
-        super8 = new Deposito();
-        trencito = new Deposito();
-        monVu = new Deposito();
+        coca = new Deposito<>();
+        sprite = new Deposito<>();
+        super8 = new Deposito<>();
+        trencito = new Deposito<>();
+        monVu = new Deposito<>();
 
         for (int i = 0; i < productos; i++) {//si el numero de bebidas es 0, no se agregan bebidas
-            coca.addBebida(new CocaCola(100 + i));
-            sprite.addBebida(new Sprite(200 + i));
-            super8.addDulce(new Super8(300+i));
-            trencito.addDulce(new Trencito(400+i));
+            coca.addDeposito(new CocaCola(100 + i));
+            sprite.addDeposito(new Sprite(200 + i));
+            super8.addDeposito(new Super8(300+i));
+            trencito.addDeposito(new Trencito(400+i));
         }
     }
     
@@ -39,83 +39,83 @@ class Expendedor{
         }
         
         if(codigo>4 || codigo<1){
-            monVu.addMoneda(m);
+            monVu.addDeposito(m);
             throw new NoHayProductoException("El numero ingresado para el codigo no es valido");
         }
 
         switch (codigo) {
             case COCA:
-                if (coca.sizeBebida() == 0) {
-                    monVu.addMoneda(m);
+                if (coca.sizeDeposito() == 0) {
+                    monVu.addDeposito(m);
                     throw new NoHayProductoException("No quedan CocaCola's, los depositos estan vacios");
 
                 } else if (precioCoca > m.getSerie().getValor()) {
-                    monVu.addMoneda(m);
+                    monVu.addDeposito(m);
                     throw new PagoInsuficienteException("Pago Insuficiente");
 
                 } else {
                     vuelto = m.getSerie().getValor() - precioCoca;
                     int numLoops = vuelto / 100;
                     for (int i = 0; i < numLoops; i++) {
-                        monVu.addMoneda(new Moneda100());
+                        monVu.addDeposito(new Moneda100());
                     }
-                    return coca.getBebida();
+                    return (Producto) coca.getDeposito();
                 }
 
             case SPRITE:
-                if (sprite.sizeBebida() == 0) {
-                    monVu.addMoneda(m);
+                if (sprite.sizeDeposito()== 0) {
+                    monVu.addDeposito(m);
                     throw new NoHayProductoException("No quedan Sprite's, los depositos estan vacios");
 
                 } else if (precioSprite > m.getSerie().getValor()) {
-                    monVu.addMoneda(m);
+                    monVu.addDeposito(m);
                     throw new PagoInsuficienteException("Pago Insuficiente");
 
                 } else {
                     vuelto = m.getSerie().getValor() - precioSprite;
                     int numLoops = vuelto / 100;
                     for (int i = 0; i < numLoops; i++) {
-                        monVu.addMoneda(new Moneda100());
+                        monVu.addDeposito(new Moneda100());
                     }
 
-                    return sprite.getBebida();
+                    return (Producto) sprite.getDeposito();
                 }
             case SUPER8:
-                if (super8.sizeDulce() == 0) {
-                    monVu.addMoneda(m);
+                if (super8.sizeDeposito() == 0) {
+                    monVu.addDeposito(m);
                     throw new NoHayProductoException("No quedan Super8's, los depositos estan vacios");
 
                 } else if (precioSuper8 > m.getSerie().getValor()) {
-                    monVu.addMoneda(m);
+                    monVu.addDeposito(m);
                     throw new PagoInsuficienteException("Pago Insuficiente");
 
                 } else {
                     vuelto = m.getSerie().getValor() - precioSuper8;
                     int numLoops = vuelto / 100;
                     for (int i = 0; i < numLoops; i++) {
-                        monVu.addMoneda(new Moneda100());
+                        monVu.addDeposito(new Moneda100());
                     }
 
-                    return super8.getDulce();
+                    return (Producto) super8.getDeposito();
 
                 }
             case TRENCITO:
-                if (trencito.sizeDulce() == 0) {
-                    monVu.addMoneda(m);
+                if (trencito.sizeDeposito() == 0) {
+                    monVu.addDeposito(m);
                     throw new NoHayProductoException("No quedan Trencito's, los depositos estan vacios");
 
                 } else if (precioTrencito > m.getSerie().getValor()) {
-                    monVu.addMoneda(m);
+                    monVu.addDeposito(m);
                     throw new PagoInsuficienteException("Pago Insuficiente");
 
                 } else {
                     vuelto = m.getSerie().getValor() - precioTrencito;
                     int numLoops = vuelto / 100;
                     for (int i = 0; i < numLoops; i++) {
-                        monVu.addMoneda(new Moneda100());
+                        monVu.addDeposito(new Moneda100());
                     }
 
-                    return trencito.getDulce();
+                    return (Producto) trencito.getDeposito();
                 }
         }
 
@@ -123,7 +123,7 @@ class Expendedor{
     }
 
     public Moneda getVuelto(){
-        Moneda mon1= monVu.getMoneda();
+        Moneda mon1= (Moneda) monVu.getDeposito();
         if(mon1==null) return null;
         else return mon1;
     }
