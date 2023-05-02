05f1817e663672b4a943e187f8bf9567f02d4fcb
mario_cso15@hotmail.com <Mario_cso15@hotmail.com>
2023-05-01 20:41:27 -0400

    Resubida Comprador preJavaDoc, por un inconvenite

--- Tarea1-2023/src/main/Comprador.java
+++ Tarea1-2023/src/main/Comprador.java
@@ -1,19 +1,11 @@
 
 package main;
-/**
-*Clase Comprador,guarda los string de los sonidos de bebidas y los retorna, devuelve el vuelto de las monedas y los retorna
-*@field sonido Guarda el string dado por las clases y subclases instanciadas y creadas en el expendedor
-*@field vuelto Guarda el vuelto total, dada por las compras     
-*/
+
+/*******Comprador*******/
 class Comprador{
     private String sonido;
     private int vuelto=0;
-    /**
-    *Metodo constructor de Comprador,calcula el vuelto y lo asocia a su atributo,ademas de asignar tambien el sonido del producto
-    *@param m Es la Moneda con la cual se compra
-    *@param cualExpende Se refiere al cual expendedor sera recibido
-    *@param exp Es el expendedor en si
-    */
+
     public Comprador(Moneda m, int cualExpende, Expendedor exp) throws PagoIncorrectoException, PagoInsuficienteException, NoHayProductoException {//1500,1400
         Producto pd= exp.comprarProducto(m,cualExpende);
         
@@ -28,17 +20,11 @@ class Comprador{
             monedaOut=exp.getVuelto();
         }
     }
-    /**
-    *retorna el vuelto a su instancia
-    *@return vuelto El vuelto almacenado
-    */
+    
     public int cuantoVuelto(){
         return vuelto;
     }
-    /**
-    *retorna el sonido a su instancia
-    *@return sonido El sonido almacenado
-    */ 
+    
     public String queDegustaste(){
         return sonido;
     }
