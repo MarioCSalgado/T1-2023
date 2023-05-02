f91a205d2cd4ce8d58d938176965586c59117b20
mario_cso15@hotmail.com <Mario_cso15@hotmail.com>
2023-05-01 20:21:44 -0400

    Modificacion Deposito a un Deposito generico

--- Tarea1-2023/src/main/Deposito.java
+++ Tarea1-2023/src/main/Deposito.java
@@ -1,66 +1,30 @@
-
 package main;
 
 import java.util.ArrayList;
 
-class Deposito{
-    private ArrayList<Bebida> be;
-    private ArrayList<Dulce> dc;
-    private ArrayList<Moneda> mon;
-    
+class Deposito<T>{
+    private ArrayList<T> deposito;
     
     public Deposito() {
-       this.be = new ArrayList<Bebida>();//deposito bebida
-       this.dc= new ArrayList<Dulce>();//deposito dulce
-       this.mon= new ArrayList<Moneda>();//deposito moneda
+       this.deposito = new ArrayList<T>();
     }
     
-    public void addBebida(Producto bebida){
-        be.add((Bebida) bebida);
+    public void addDeposito(T producto){
+        deposito.add(producto);
+    }
+
+    
+    public int sizeDeposito(){
+        return deposito.size();
     }
     
-    public void addDulce(Producto dulce){
-        dc.add((Dulce) dulce);
-    }
-    
-    public int sizeBebida(){
-        return be.size();
-    }
-      public int sizeDulce(){
-        return dc.size();
-    }
-    
-    public Bebida getBebida(){
-       int tam1= be.size();
-       if(tam1!=0){ 
-       Bebida b= be.remove(0);
-       return b;
-       }else{
-           return null;
-       }
-    }
-    
-    public Dulce getDulce(){
-       int tam2= dc.size();
-       if(tam2!=0){ 
-       Dulce d= dc.remove(0);
-       return d;
-       }else{
-           return null;
-       }
-    }
-    
-    public void addMoneda(Moneda moneda){
-        mon.add(moneda);
-    }
-    
-    public Moneda getMoneda(){
-       int tam= mon.size();
+    public T getDeposito(){
+       int tam= deposito.size();
        if(tam!=0){ 
-       Moneda mo= mon.remove(0);
-       return mo;
+       T producto= deposito.remove(0);
+       return producto;
        }else{
            return null;
        }
-    } 
+    }
 }
